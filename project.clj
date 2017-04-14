1e851dfa46f382ca2f40f6de19a0420c1ad721ff
Shafeeq Kunnakkadan <shafeeq@nilenso.com>
2017-04-14 12:34:43 +0530

    Bump to non-snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-crud "0.1.0-SNAPSHOT"
+(defproject re-crud "0.1.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0"]
