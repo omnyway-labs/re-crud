ed6ba8dec6989fccc39a345ab8266649d031d259
Robert J. Berger <rberger@ibd.com>
2017-05-01 18:22:29 -0700

    Updated all files with license

--- example-app/src/cljs/re_crud_example/view/user.cljs
+++ example-app/src/cljs/re_crud_example/view/user.cljs
@@ -1,3 +1,20 @@
+;;
+;; re-crud - A crud library for re-frame apps
+;;
+;; Copyright 2017 Omnyway Inc.
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;   http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+;;
 (ns re-crud-example.view.user
   (:refer-clojure :exclude [list update])
   (:require [re-frame.core :as re-frame :refer [reg-event-fx dispatch subscribe]]
